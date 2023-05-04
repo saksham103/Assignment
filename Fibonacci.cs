fb1ca93001fe8cfd9f227698872469fbe1baa5f1
saksham103 <72139713+saksham103@users.noreply.github.com>
2023-05-04 15:29:08 +0530

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3, i, number;
+            Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 +" "+ n2);
+            for (i = 2; i <= number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write (" " + n3);
+                n1 = n2;
+                n2= n3;
+            }
+            
+        }
+    }
+}
